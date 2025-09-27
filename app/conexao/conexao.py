c8eba4e9809d5a859b8d1e24aea6c45b3c62d80b
Gabs (Linux) <gabriel.daniel@sou.ufac.br>
2025-09-26 22:41:41 -0500

    FIX: retirando o banco de dados de dentro do /app para prevenção de conflitos de arquivos | REF: #4

--- app/conexao/conexao.py
+++ app/conexao/conexao.py
@@ -3,7 +3,7 @@ from sqlite3 import Error
 
 class Conexao:
     def get_conexao(self):
-        caminho = 'app/conexao/db'
+        caminho = 'db'
         try:
             con = sqlite3.connect(caminho)
             return con

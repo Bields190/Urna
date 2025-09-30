db8d01f1bcd165ca0774cc8ba60bf78950f3d90b
Andrey-Marques <andreymarques958@gmail.com>
2025-09-30 10:58:25 -0500

    FIX: tamanhos e cores alterados #6

--- app/view/telaEleicoes.py
+++ app/view/telaEleicoes.py
@@ -17,13 +17,13 @@ class Tela:
         self.iconeHamburguer = ImageTk.PhotoImage(img)
         
         
-        self.btn = tk.Button(self.janela, image=self.iconeHamburguer, command=self.mostrarMenu)
+        self.btn = tk.Button(self.janela, image=self.iconeHamburguer, command=self.mostrarMenu, bg="white")
         self.btn.image = self.iconeHamburguer  
         self.btn.grid(row=0, column=0, sticky="nw", padx=10, pady=10)
         
         self.menu = Menu(self.janela, tearoff=0)
 
-        self.menu.add_command(label="Opção 20")
+        self.menu.add_command(label="Logout")
 
     
 #-----sessão das eleições----------
@@ -31,13 +31,13 @@ class Tela:
         self.janela.rowconfigure(3, weight=3)
         self.janela.rowconfigure(3, weight=1)
         
-        self.lbl_ola = tk.Label(text="Olá! Administrador",font=("Arial",20,"bold"))
+        self.lbl_ola = tk.Label(text="Olá! Administrador",font=("Arial",20,"bold"), bg="white")
         self.lbl_ola.grid(row=1,column=0, pady=(40, 10), padx=(20,0))
         
-        self.btn_criar_eleicao = tk.Button(text="+ Criar Nova Eleição", font=("Arial",16,"bold"))
+        self.btn_criar_eleicao = tk.Button(text="+ Criar Nova Eleição", font=("Arial",16,"bold"), fg="white",bg="black")
         self.btn_criar_eleicao.grid(row=2,column=0,pady=(30,60))
 #-----sessão 2 das eleições----------      
-        self.frmEleicoes = tk.Frame(self.janela, bd=2, relief="solid", padx=5, pady=5)
+        self.frmEleicoes = tk.Frame(self.janela, bd=2, padx=5, pady=5,bg="white",relief="solid")
         self.frmEleicoes.grid(row=3, column=0, columnspan=3, padx=80, pady=(20,100), sticky="nsew")
         self.frmEleicoes.columnconfigure(0, weight=1)
         self.frmEleicoes.columnconfigure(1, weight=1)
@@ -45,43 +45,39 @@ class Tela:
 
 #essas eleicoes sao apenas exemplos para dimensionar a tela
 #-----eleição 1------
-        eleicao1 = tk.Frame(self.frmEleicoes, bd=2, relief="solid", width=100, height=400)
+        eleicao1 = tk.Frame(self.frmEleicoes, bd=2, relief="solid", width=100, height=400,bg="white")
         eleicao1.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
         eleicao1.grid_propagate(False)  
         eleicao1.pack_propagate(False)  
 
-
-        tk.Label(eleicao1, text="Eleição 15", font=("Arial", 12, "bold"), fg="black").pack(anchor="nw", side="top")
-        tk.Label(eleicao1, text="Ativa", bg="green", fg="white", font=("Arial", 10, "bold")).pack(fill="x", pady=2, side="bottom")
-        tk.Button(eleicao1, text="Encerrar").pack(fill="x", pady=2, side="bottom")
-        tk.Button(eleicao1, text="Abrir Urna").pack(fill="x", pady=2, side="bottom")
-        tk.Label(eleicao1, text="●", fg="green", font=("Arial", 10, "bold")).pack(anchor="ne", side="bottom")
+        tk.Label(eleicao1, text="Eleição 15", font=("Arial", 15, "bold"), fg="black",padx=15,pady=15,bg="white").pack(anchor="nw", side="top")
+        tk.Label(eleicao1, text="Ativa", bg="green", fg="white", font=("Arial", 13, "bold"),height=3).pack(fill="x", pady=5, side="bottom",padx=10)
+        tk.Button(eleicao1, text="Encerrar",font=("Arial", 13, "bold"),height=3, bg="white",relief="solid").pack(fill="x", pady=5, side="bottom",padx=10)
+        tk.Button(eleicao1, text="Abrir Urna",font=("Arial", 13, "bold"),height=3, bg="white",relief="solid").pack(fill="x", pady=5, side="bottom",padx=10)
         
-
+'''
 
 #-----eleição 2------
-        eleicao2 = tk.Frame(self.frmEleicoes, bd=2, relief="solid", width=100, height=400)
+        eleicao2 = tk.Frame(self.frmEleicoes, bd=2, relief="solid", width=100, height=400,bg="white")
         eleicao2.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
         eleicao2.grid_propagate(False)
         eleicao2.pack_propagate(False)
 
-        tk.Label(eleicao2, text="Eleição 14", font=("Arial", 12, "bold"), fg="black").pack(anchor="nw")
-        tk.Label(eleicao2, text="●", fg="red", font=("Arial", 10, "bold")).pack(anchor="ne")
-        tk.Button(eleicao2, text="Resultados").pack(fill="x", pady=2)
-        tk.Button(eleicao2, text="Arquivar").pack(fill="x", pady=2)
-        tk.Label(eleicao2, text="Encerrada", bg="red", fg="white", font=("Arial", 10, "bold")).pack(fill="x", pady=2)
+        tk.Label(eleicao2, text="Eleição 14", font=("Arial", 15, "bold"), fg="black",padx=15,pady=15,bg="white").pack(anchor="nw", side="top")
+        tk.Label(eleicao2, text="Encerrada", bg="red", fg="white", font=("Arial", 13, "bold"),height=3).pack(fill="x", pady=5, side="bottom",padx=10)
+        tk.Button(eleicao2, text="Arquivar",font=("Arial", 13, "bold"),height=3, bg="white",relief="solid").pack(fill="x", pady=5, side="bottom",padx=10)
+        tk.Button(eleicao2, text="Resultados",font=("Arial", 13, "bold"),height=3, bg="white",relief="solid").pack(fill="x", pady=5, side="bottom",padx=10)
 
 #-----eleição 3------
-        eleicao3 = tk.Frame(self.frmEleicoes, bd=2, relief="solid", width=100, height=400)
+        eleicao3 = tk.Frame(self.frmEleicoes, bd=2, relief="solid", width=100, height=400,bg="white")
         eleicao3.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
         eleicao3.grid_propagate(False)
         eleicao3.pack_propagate(False)
 
-        tk.Label(eleicao3, text="Eleição 13", font=("Arial", 12, "bold"), fg="black").pack(anchor="nw")
-        tk.Label(eleicao3, text="●", fg="red", font=("Arial", 10, "bold")).pack(anchor="ne")
-        tk.Button(eleicao3, text="Resultados").pack(fill="x", pady=2)
-        tk.Button(eleicao3, text="Arquivar").pack(fill="x", pady=2)
-        tk.Label(eleicao3, text="Encerrada", bg="red", fg="white", font=("Arial", 10, "bold")).pack(fill="x", pady=2)
+        tk.Label(eleicao3, text="Eleição 13", font=("Arial", 15, "bold"), fg="black",padx=15,pady=15,bg="white").pack(anchor="nw", side="top")
+        tk.Label(eleicao3, text="Encerrada", bg="red", fg="white", font=("Arial", 13, "bold"),height=3).pack(fill="x", pady=5, side="bottom",padx=10)
+        tk.Button(eleicao3, text="Arquivar",font=("Arial", 13, "bold"),height=3,bg="white",relief="solid").pack(fill="x", pady=5, side="bottom",padx=10)
+        tk.Button(eleicao3, text="Resultados",font=("Arial", 13, "bold"),height=3,bg="white",relief="solid").pack(fill="x", pady=5, side="bottom",padx=10)'''
      
 gui = tk.Tk()
 Tela(gui)
